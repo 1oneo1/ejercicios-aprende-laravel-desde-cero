e56c5e62931796cdce72a04a873ff68533cbd1f1
asiersodupe <oneo6556@gmail.com>
2025-08-20 20:27:39 +0200

    Solución ejercicio 2

--- routes/web.php
+++ routes/web.php
@@ -34,3 +34,58 @@ Route::post('/ejercicio1', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+// Route::get('/contact', function () {
+//     return Response::view('contact');
+// });
+
+// PHP 8.0+
+Route::get('/contact', fn () => Response::view('contact'));
+
+Route::post('/contact', function (Request $request) {
+    // dd($request);
+    return Response::json(["message" => "Hola"])->setStatusCode(400);
+});
+
+Route::post('/ejercicio2/a', function (Request $request) {
+    return Response::json($request);
+});
+
+Route::post('/ejercicio2/b', function (Request $request) {
+    $price = $request->get('price');
+    if (!is_numeric($price) || $price <= 0) {
+        return Response::json(["message" => "Price can't be less than 0"])->setStatusCode(422);
+    }
+
+    return Response::json($request);
+});
+
+Route::post('/ejercicio2/c', function (Request $request) {
+
+    $discountPor = 0;
+    $price = $request->get('price');
+
+    if ($request->query->has('discount')) {
+        $discount = $request->get('discount');
+        switch ($discount) {
+            case 'SAVE5':
+                $price = $price - $price * 0.05;
+                $discountPor = 5;
+                break;
+            case 'SAVE10':
+                $price = $price - $price * 0.10;
+                $discountPor = 10;
+                break;
+            case 'SAVE15':
+                $price = $price - $price * 0.15;
+                $discountPor = 15;
+                break;
+        }
+    }
+
+    $response = $request->all();
+    $response['price'] = $price;
+    $response['discount'] = $discountPor;
+
+    return Response::json($response);
+});
